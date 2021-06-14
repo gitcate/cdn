742adc35246b1b1da8cf15b5a282b29dcbcfa17a
gitcate <gitbak@789987.xyz>
2021-06-14 17:21:36 +0800

    New

---
+++ js/musicDB.js
@@ -0,0 +1,33 @@
+//name		歌曲名称			是
+//artist	艺术家数组或文本	否
+//cover		封面链接			否
+//lyrics	歌词文本			否
+//provider	提供器名称			否，默认 default
+//lrc		歌词链接			否
+window.QPlayer.list = [
+	{
+        "name": "Uptown Funk",
+        "artist": "Mark Ronson",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3"
+    },
+    {
+        "name": "童话镇",
+        "artist": "陈一发儿",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+    },
+    {
+        "name": "护花使者",
+        "artist": "李克勤",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/李克勤_护花使者.mp3",
+    },
+    {
+        "name": "城里的月光",
+        "artist": "许美静",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_城里的月光.mp3",
+    },
+    {
+        "name": "信仰",
+        "artist": "张信哲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张信哲_信仰.mp3",
+    }
+];
