9a7771cf759a610fad5b017713edeb8ae5f526b9
gitcate <gitbak@789987.xyz>
2021-06-19 18:03:50 +0800

    New

--- js/musicDB.js
+++ js/musicDB.js
@@ -5,35 +5,25 @@
 //provider	提供器名称			否，默认 default
 //lrc		歌词链接			否
 window.QPlayer.list = [
-	{
-        "name": "光阴的故事",
-        "artist": "罗大佑",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/罗大佑_光阴的故事.mp3"
-    },
-	{
-        "name": "故乡",
-        "artist": "许巍",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许巍_故乡.mp3"
-    },
-	{
-        "name": "都是夜归人",
-        "artist": "许美静",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_都是夜归人.mp3"
-    },
-	{
-        "name": "Andy",
-        "artist": "阿杜",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_Andy.mp3"
-    },
-	{
-        "name": "Uptown Funk",
-        "artist": "Mark Ronson",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3"
+    {
+        "name": "Louder",
+        "artist": "Charice",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Charice_Louder.mp3",
     },
     {
-        "name": "童话镇",
-        "artist": "陈一发儿",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+        "name": "Uptown Funk",
+        "artist": "Mark Ronson",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3",
+    },
+    {
+        "name": "เจอกับตัวเอง...ถึงรู้",
+        "artist": "Sara Nusara Poongprasert",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Sara Nusara Poongprasert_เจอกับตัวเอง...ถึงรู้.mp3",
+    },
+    {
+        "name": "信仰",
+        "artist": "张信哲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张信哲_信仰.mp3",
     },
     {
         "name": "护花使者",
@@ -41,23 +31,43 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/李克勤_护花使者.mp3",
     },
     {
+        "name": "月满西楼",
+        "artist": "童丽",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/童丽_月满西楼.mp3",
+    },
+    {
+        "name": "光阴的故事",
+        "artist": "罗大佑",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/罗大佑_光阴的故事.mp3",
+    },
+    {
+        "name": "故乡",
+        "artist": "许巍",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许巍_故乡.mp3",
+    },
+    {
         "name": "城里的月光",
         "artist": "许美静",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_城里的月光.mp3",
     },
-	{
+    {
+        "name": "都是夜归人",
+        "artist": "许美静",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/许美静_都是夜归人.mp3",
+    },
+    {
+        "name": "Andy",
+        "artist": "阿杜",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_Andy.mp3",
+    },
+    {
+        "name": "童话镇",
+        "artist": "陈一发儿",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+    },
+    {
         "name": "无地自容",
         "artist": "黑豹乐队",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/黑豹乐队_无地自容.mp3",
     },
-    {
-        "name": "Louder",
-        "artist": "Charice",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Charice_Louder.mp3",
-    },
-    {
-        "name": "信仰",
-        "artist": "张信哲",
-        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/张信哲_信仰.mp3",
-    }
 ];
