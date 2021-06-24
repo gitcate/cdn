cf8fae30cf777120bbf33eaa01ae054451dd6c38
gitcate <gitbak@789987.xyz>
2021-06-24 20:19:20 +0800

    New

--- js/musicDB.js
+++ js/musicDB.js
@@ -11,6 +11,11 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Charice_Louder.mp3",
     },
     {
+        "name": "Hotel California",
+        "artist": "Eagles",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Eagles_Hotel California.mp3",
+    },
+    {
         "name": "I Hate Myself For Loving You",
         "artist": "Joan Jett",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Joan Jett_I Hate Myself For Loving You.mp3",
@@ -21,6 +26,11 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Mark Ronson_Uptown Funk.mp3",
     },
     {
+        "name": "Beat It",
+        "artist": "Michael Jackson",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Michael Jackson_Beat It.mp3",
+    },
+    {
         "name": "Brother Louie",
         "artist": "Modern Talking",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Modern Talking_Brother Louie.mp3",
@@ -29,6 +39,16 @@ window.QPlayer.list = [
         "name": "เจอกับตัวเอง...ถึงรู้",
         "artist": "Sara Nusara Poongprasert",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/Sara Nusara Poongprasert_เจอกับตัวเอง...ถึงรู้.mp3",
+    },
+    {
+        "name": "恋人未满",
+        "artist": "SHE",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/SHE_恋人未满.mp3",
+    },
+    {
+        "name": "浪人情歌",
+        "artist": "伍佰",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/伍佰_浪人情歌.mp3",
     },
     {
         "name": "世界第一等",
@@ -51,14 +71,34 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/李克勤_护花使者.mp3",
     },
     {
+        "name": "执迷不悔",
+        "artist": "王菲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/王菲_执迷不悔.mp3",
+    },
+    {
+        "name": "笑忘书",
+        "artist": "王菲",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/王菲_笑忘书.mp3",
+    },
+    {
         "name": "月满西楼",
         "artist": "童丽",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/童丽_月满西楼.mp3",
     },
     {
+        "name": "耶利亚女郎",
+        "artist": "童安格",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/童安格_耶利亚女郎.mp3",
+    },
+    {
         "name": "光阴的故事",
         "artist": "罗大佑",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/罗大佑_光阴的故事.mp3",
+    },
+    {
+        "name": "失恋",
+        "artist": "草蜢",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/草蜢_失恋.mp3",
     },
     {
         "name": "许愿池的希腊少女",
@@ -86,6 +126,11 @@ window.QPlayer.list = [
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_Andy.mp3",
     },
     {
+        "name": "天黑",
+        "artist": "阿杜",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_天黑.mp3",
+    },
+    {
         "name": "撕夜",
         "artist": "阿杜",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/阿杜_撕夜.mp3",
@@ -94,6 +139,11 @@ window.QPlayer.list = [
         "name": "童话镇",
         "artist": "陈一发儿",
         "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈一发儿_童话镇.mp3",
+    },
+    {
+        "name": "梦醒时分",
+        "artist": "陈淑华",
+        "audio": "https://cdn.jsdelivr.net/gh/gitcate/cdn/music/陈淑华_梦醒时分.mp3",
     },
     {
         "name": "伤感的恋人",
